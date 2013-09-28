1ed1f1a4cb6111e25965fb157a6dfce2f65d53f2
hhd418 <hhd418@gmail.com>
2013-09-28 13:43:16 -0500

    Use migration to add a gif column and description column to the movies model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130928184018) do
+ActiveRecord::Schema.define(version: 20130928184200) do
 
   create_table "movies", force: true do |t|
     t.string   "movie"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20130928184018) do
     t.string   "trailier"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "gif"
+    t.string   "description"
   end
 
 end
