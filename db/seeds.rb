f2e63548e526f627d3cb7cd5f3473628c98b970f
hhd418 <hhd418@gmail.com>
2013-09-28 14:24:06 -0500

    Add first movie info to seed file. Test to make sure it works. Seed file deletes existing table data and replaces with seed file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+puts "clearing data in the Movies table"
+
+Movie.destroy_all
+
+puts "adding dummy data for Movies table"
+
+Movies = [
+  {
+    movie: 'matrix',
+    title: 'The Matrix',
+    gif: 'http://25.media.tumblr.com/96304ccd2c3b8bb9d22d55f650499f5f/tumblr_mn46179Vff1rnvb0co1_500.gif',
+    trailer_id: 'm8e-FF8MsqU',
+    description: 'A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.'
+  }
+]
+
+Movies.each do |movie|
+  Movie.create(movie: movie[:movie], title: movie[:title], gif: movie[:gif], trailer_id: movie[:trailer_id], description: movie[:description])
+end
+
+puts "added dummy data for movies table"
