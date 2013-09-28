8fa12094c3992a709449f9200aa5c5f11fa18e6c
hhd418 <hhd418@gmail.com>
2013-09-28 12:38:08 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+URBD::Application.config.secret_key_base = 'ab1d9fcd890ac61238726cd868da6b707e350065068bca4fd2732ca78f9c5a4048dcbbd7b05bf63d481d29724547bffacb2db6095105efec60554ffca2fca1fd'
